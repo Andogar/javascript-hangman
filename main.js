18a738a435bd93a3f6b954b2d2680717123c1f38
Andogar <robert.andoga@gmail.com>
2017-06-13 08:51:19 -0400

    Working buttons added to add values to the html page

--- main.js
+++ main.js
@@ -28,34 +28,46 @@ var commonWords = [
 var chooseRandomWord = function(array) {
   return array[Math.floor(Math.random() * Math.max(array.length - 1), 0)];
 }
-
 var chosenWord = chooseRandomWord(commonWords);
 var answerWord = chosenWord;
+
+var letterInput = document.querySelectorAll('button');
+var correctLettersHTML = document.querySelector(".correct-letters");
+var incorrectLettersHTML = document.querySelector(".attempts");
+var answer = document.querySelector(".results");
+
+var incorrectCharacters = [];
+for (var i = 0; i < letterInput.length; i++) {
+  letterInput[i].onclick = function(event) {
+    var letterValue;
+    letterValue = " " + this.value;
+    incorrectCharacters.push(letterValue);
+    incorrectLettersHTML.innerHTML = incorrectCharacters;
+  }
+}
+
 var counter = 10;
-var triedCharacters = [];
+var incorrectCharacters = [];
 var correctCharacters = [];
 var counterCheck = 0;
 // Create a function that accepts a single character argument
 var checkForCharacter = function(character) {
-  var result = false;
-  triedCharacters.push(character);
-  counterCheck++
-  console.log("You have tried these characters: " + triedCharacters);
   if (counterCheck <= counter) {
     for (var i = 0; i < chosenWord.length; i++) {
       if (character == chosenWord[i]) {
-        result = true;
         correctCharacters.push(character);
         chosenWord = chosenWord.slice(0, i) + chosenWord.slice(i+1);
-        console.log("These characters are correct: " + correctCharacters);
+        return "These characters are correct: " + correctCharacters;
         if (chosenWord == "") {
-          console.log("You did it! The correct word was " + answerWord + ".")
+          return "You did it! The correct word was " + answerWord + "."
         }
       } else {
-        //
+        incorrectCharacters.push(character);
+        counterCheck++
+        console.log("You have tried these characters: " + incorrectCharacters);
+
       }
     }
-    return result;
   } else {
     return "You've run out of guesses";
   }
